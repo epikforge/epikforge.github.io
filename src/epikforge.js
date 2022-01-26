350202af61e2e7efa99ce09f6df4432f8d5521d2
Jozsef Hocza <jozsef@hocza.com>
2022-01-26 21:17:04 +0100

    add docs, and new feature: zip to city

--- src/epikforge.js
+++ src/epikforge.js
@@ -3,6 +3,35 @@ let input = document.querySelector('input[name="m"]');
 let params_m = params.get('m');
 if(input && params_m) input.value = params_m;
 
+function delay(callback, ms) {
+    var timer = 0;
+    return function() {
+        var context = this, args = arguments;
+        clearTimeout(timer);
+        timer = setTimeout(function () {
+            callback.apply(context, args);
+        }, ms || 0);
+    };
+}
+
+let zip = document.querySelector('input[data-zip]');
+let city = document.querySelector('input[data-city]');
+if(zip && city) {
+    let country = zip.getAttribute('data-country');
+    if(country === null) country = 'hu';
+    // add event listener with delay
+    zip.addEventListener('input', delay(function(e) {
+        let zip = e.target.value;
+        let url = 'https://api.zippopotam.us/' + country + '/' + zip;
+        fetch(url)
+            .then(function(response) {
+                return response.json();
+            })
+            .then(function(data) {
+                city.value = data.places[0]['place name'];
+            });
+    }, 500),500);
+}
 var smoothscroll = require('smoothscroll-polyfill');
 smoothscroll.polyfill();
 
